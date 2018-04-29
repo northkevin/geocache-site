642d10891144fd2de5132f8835a5c86a305a0473
Kevin <github@northkevin.com>
2018-04-28 18:01:38 -0700

    added bootstrap scripts, worked on geocache input form

--- geocache.php
+++ geocache.php
@@ -15,6 +15,9 @@
 
     <script src="http://u.arizona.edu/~lxu/cscv337/sp18/hw6/js/prototype.js" type="text/javascript"></script>
     <script src="http://u.arizona.edu/~lxu/cscv337/sp18/hw6/js/scriptaculous.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
 
   <body>
@@ -29,65 +32,140 @@
     <div id="main">
       <!-- a place to inject the results from actors.php -->
 
-      <div id="geocache-form-inputs"> 
-        <form id="geocache-form" method="post" action="query.php">
-          <fieldset>
-            <legend> Geocache Locator </legend>
-            <p> Click on the map to select a location, or enter one manually below. </p>
+      <div class="container">
 
-            <div class="form-group row">
-              <label for="inputLatitude" class="col-sm-2 col-form-label">Latitude:</label>
-              <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputLatitude" placeholder="latitude">
-              </div>
+        <!-- row 1 -->
+        <div class="row">
+
+          <!-- col 1 -->
+          <div class="col-md" id="geocache-form-inputs">
+            <div class="row justify-content-center">
+              <form class="needs-validation" id="geocache-form" method="post" action="query.php" novalidate>
+                <fieldset>
+
+                <div class="form-row justify-content-center">
+                  <div class="form-group">
+                    <label class="title" for="form-instructions">Geocache Locator</label>
+                    <small id="form-instructions" class="form-text">Click on the map to select a location, or enter one manually below.</small>
+                  </div>
+                </div>
+                
+                <div class="form-row justify-content-center">
+                  <div class="form-group col-md-4 justify-content-left">
+                    <label for="inputLatitude">Latitude:</label>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="inputLatitude" placeholder="latitude" required>
+                  </div>
+                  <div class="invalid-feedback">
+                    Please provide a valid Latitude.
+                  </div>
+                </div>
+
+                <div class="form-row justify-content-center">
+                  <div class="form-group col-md-4 justify-content-left">
+                    <label for="inputLongitude">Longitude:</label>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="inputLongitude" placeholder="longitude" required>
+                  </div>
+                  <div class="invalid-feedback">
+                    Please provide a valid Longitude.
+                  </div>
+                </div>
+
+                <div class="form-row justify-content-center">
+                  <div class="form-group col-md-4 justify-content-left">
+                    <label for="inputRadius">Radius (miles):</label>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="inputRaidus" placeholder="radius" required>
+                  </div>
+                  <div class="invalid-feedback">
+                    Please provide a valid Radius.
+                  </div>
+                </div>
+
+                <div class="form-group row justify-content-center">
+                  <div class="form-group col-md-4 justify-content-left">
+                    <label for="selectCacheType">Cache Type:</label>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <select class="form-control" id="selectCacheType" placeholder="cache-type">
+                      <option>opt 1</option>
+                      <option>opt 2</option>
+                      <option>opt 3</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="form-group row justify-content-center">
+                  <div class="form-group col-md-4 justify-content-left">
+                    <label for="selectDifficulty">Difficulty:</label>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <select class="form-control" id="selectDifficulty" placeholder="difficulty">
+                      <option>opt 1</option>
+                      <option>opt 2</option>
+                      <option>opt 3</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="form-group row justify-content-center">
+                  <div class="form-group col-md-6">
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                  </div>
+                </div>
+
+                
+              </fieldset>
+            </form>
             </div>
+            
 
-            <div class="form-group row">
-              <label for="inputLongitude" class="col-sm-2 col-form-label">Longitude:</label>
-              <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputLongitude" placeholder="longitude">
-              </div>
-            </div>
+            <script>
+            // Example starter JavaScript for disabling form submissions if there are invalid fields
+            (function() {
+              'use strict';
+              window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                  form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                      event.preventDefault();
+                      event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                  }, false);
+                });
+              }, false);
+            })();
+            </script>
+          </div>
 
-            <div class="form-group row">
-              <label for="inputRadius" class="col-sm-2 col-form-label">Radius (miles):</label>
-              <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputRaidus" placeholder="radius">
-              </div>
-            </div>
+          <!-- col 2 -->
+          <div class="col-md" id="google-maps-api"> 
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label for="selectCacheType" class="col-sm-2 col-form-label">Cache Type:</label>
-              <div class="col-sm-10">
-                <select class="form-control" id="selectCacheType" placeholder="cache-type">
-                  <option>opt 1</option>
-                  <option>opt 2</option>
-                  <option>opt 3</option>
-                </select>
-              </div>
-            </div>
+        <!-- row 2 -->
+        <div class="row">
 
-            <div class="form-group row">
-              <label for="selectDifficulty" class="col-sm-2 col-form-label">Difficulty:</label>
-              <div class="col-sm-10">
-                <select class="form-control" id="selectDifficulty" placeholder="difficulty">
-                  <option>opt 1</option>
-                  <option>opt 2</option>
-                  <option>opt 3</option>
-                </select>
-              </div>
-            </div>
+          <!-- col 1 -->
+          <div class="col-md" id="geocache-form-results">
 
-            <button class="btn btn-primary" type="submit">Submit</button>
-          </fieldset>
-        </form>
+          </div>
+        </div>
       </div>
 
-      <div id="geocache-form-results"> 
+      
 
-      </div>
+      
 
-      <div id="google-maps-api"> </div>
+      
 
       <div id="flickr-api"> </div>
 
